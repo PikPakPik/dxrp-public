047f842d3189a4fd54c9675be9ab0f5d01ba5d27
Alexandre T <alexandre.tressel@epitech.eu>
2026-06-30 00:53:01 +0200

    Add TV line of sight setting and update playback logic

--- game/Code/Entity/Entities/TvEntity.cs
+++ game/Code/Entity/Entities/TvEntity.cs
@@ -29,6 +29,9 @@ public class TvEntity : BaseEntity, IDescription, Component.IPressable
 	[ConVar( "dx_tv_range", ConVarFlags.Saved, Min = 0, Max = 5000 )]
 	private static int TvRange { get; set; } = 600;
 
+	[ConVar( "dx_tv_line_of_sight", ConVarFlags.Saved )]
+	private static bool RequireLineOfSight { get; set; }
+
 	// True if the TV is currently active and playing content
 	private bool _isCurrentlyPlaying;
 
@@ -85,14 +88,48 @@ public class TvEntity : BaseEntity, IDescription, Component.IPressable
 	{
 		_lastCheck = 0;
 		var playerPosition = Player.Local.Controller.EyePosition;
-		var inRange = playerPosition.Distance( WorldPosition ) <= TvRange;
+		var playerDistance = playerPosition.Distance( WorldPosition );
+
+		if ( RequireLineOfSight )
+		{
+			// Only do raycast check if we're within basic distance range
+			if ( playerDistance <= TvRange )
+			{
+				// Perform raycast from player to TV to check for obstacles
+				var originPos = WorldPosition + WorldRotation * (Vector3.Up * 30 * WorldScale.z) + WorldRotation * Vector3.Forward * 5;
+				var direction = (originPos - playerPosition).Normal;
+
+				var trace = Scene.Trace.Ray( new Ray( playerPosition, direction ), TvRange )
+					.WithTag( "solid" )
+					.WithoutTags( Constants.PlayerTag )
+					.Run();
+
+				var isVisible = trace.GameObject == GameObject;
+
+				switch ( _isCurrentlyPlaying )
+				{
+					case false when playerDistance <= TvRange && isVisible && HasVideoContent():
+						StartPlayback();
+						break;
+					case true when playerDistance > TvRange || !isVisible:
+						StopPlayback();
+						break;
+				}
+			}
+			else if ( _isCurrentlyPlaying )
+			{
+				StopPlayback();
+			}
+
+			return;
+		}
 
 		switch ( _isCurrentlyPlaying )
 		{
-			case false when inRange && HasVideoContent():
+			case false when playerDistance <= TvRange && HasVideoContent():
 				StartPlayback();
 				break;
-			case true when !inRange:
+			case true when playerDistance > TvRange:
 				StopPlayback();
 				break;
 		}
