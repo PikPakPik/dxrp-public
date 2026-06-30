c9ddcab67ca8e579171ddec18115d2643f9b9e9d
Dimmer <223179705+dimmerly@users.noreply.github.com>
2026-06-30 20:12:09 +1000

    Fix shipment names

--- game/Code/Entity/Entities/ShipmentEntity.cs
+++ game/Code/Entity/Entities/ShipmentEntity.cs
@@ -34,18 +34,7 @@ public class ShipmentEntity : BaseEntity, IWireUsable, Component.IPressable
 	public override bool DestroyOnJobChange => false;
 	public override bool AllowOwnershipTransfer => true;
 
-	public override string DisplayName
-	{
-		get
-		{
-			var name = GameModeEntity.DisplayName();
-			if ( name.StartsWith( '#' ) )
-			{
-				name = Language.GetPhrase( name[1..] );
-			}
-			return $"{name} ({QuantityText.Text})";
-		}
-	}
+	public override string DisplayName => $"{TypeText.Text} ({QuantityText.Text})";
 
 	private bool _occluded;
 
