2efadc6c965da1ca09b92712d52e8693f942a431
Alexandre T <alexandre.tressel@epitech.eu>
2026-07-02 18:38:14 +0200

    Show label when wire is deployed

--- game/Code/Construct/Wire/BaseWireConstruct.cs
+++ game/Code/Construct/Wire/BaseWireConstruct.cs
@@ -1,3 +1,4 @@
+using Dxura.RP.Game.Tools;
 using Dxura.RP.Game.UI;
 namespace Dxura.RP.Game.Wire;
 
@@ -16,7 +17,7 @@ public abstract class BaseWireConstruct( ConstructType type ) : BaseConstruct( t
 	public Vector3 ContextPosition => WorldPosition + Vector3.Up * 6f;
 	public bool LookOpacity => false;
 	public float ContextMaxDistance => 100f;
-	public virtual bool ShouldShow() => !string.IsNullOrWhiteSpace( DisplayText );
+	public virtual bool ShouldShow() => (WireTool.IsDeployed || WireLabelerTool.IsDeployed) && !string.IsNullOrWhiteSpace( DisplayText );
 
 	protected override void OnStart()
 	{
